6c9df682545f307dda2d5732aca4049101591d9b
ada2 <87185550+2ADA2@users.noreply.github.com>
2023-11-25 01:33:35 +0300

    counter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    if(count<0) setCount(0);
+  });
+  return(
+    <div className = 'counter'>
+      <h1>Счётчик</h1>
+      <h2>{count}</h2>
+      <div className = 'control-count'>
+        <button className='button less' onClick={() => setCount(count - 1)}>уменьшить</button>      
+        <button className = 'button more' onClick = {() => setCount(count + 1)}>увеличить</button>
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
