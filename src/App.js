c908edb672124ad2700b5efde2fce59f02346f15
ada2 <87185550+2ADA2@users.noreply.github.com>
2023-12-09 02:11:02 +0300

    userList"

--- src/App.js
+++ src/App.js
@@ -1,100 +1,116 @@
 import './App.css';
 import React from 'react';
-import questions from './questions.json'
+import Users from "./users.json"
+let users = Users.users;
+users.map((elem) => elem.offer = true);
 
-
-const progress = React.createRef();
-
-function Answer(props){
-  return <button className='Quiz-answer button' 
-    onClick={() => props.click(props.question)}
-  >{props.question}</button>
-}
-
-function Question(props){
-  const finished = props.finished + '%';
+function User(props){
   return (
-    <div className = 'Quiz-section'>
-      <div className = "Quiz-container">
-        <div className = 'progressBar'>
-          <div className = 'progress' ref = {progress} style = {{width: finished}}></div>
-        </div>
-        <div className = 'question'>{props.question}</div>
-        <div className = 'Quiz-answers'>
-          <Answer question={props.questions[0]} click = {(answ) => props.click(answ)}/>
-          <Answer question={props.questions[1]} click = {(answ) => props.click(answ)} />
-          <Answer question={props.questions[2]} click = {(answ) => props.click(answ)} />
-        </div>
+    <div className = "user">
+      <img  className = "user-ava" src= "https://i.natgeofe.com/n/4f5aaece-3300-41a4-b2a8-ed2708a0a27c/domestic-dog_thumb_square.jpg"></img>
+      <div className = 'info'>
+        <p className = 'userName'>{props.name}</p>
+        <p className = 'userMail'>{props.email}</p>
       </div>
-
-      <div id = 'section-decoration1' className = 'section-decoration'>
-        <div id = 'section-decoration2' className = 'section-decoration'></div>
-      </div>
+      {(props.inList) ? 
+        <button className = 'offer' onClick = {() => {props.changeStatus(props.num)}}>
+        {(props.offer) ? '+' : '-'}
+        </button> : <></>
+      }
+      
     </div>
   )
 }
 
-function End (props) {
-  props.finish()
-  return (
-    <div className='Quiz-section'>
-      <div className="Quiz-container">
-        <div className='progressBar'>
-          <div className='progress progress-end' ref={progress} style = {{width:`${props.finished + '%'}`}}></div>
-        </div>
-        <div className = 'result-container'>
-          <h2>Результат:</h2>
-          <div className = 'result'>{props.result}%</div>
-        </div>
-      </div>
-      <div id='section-decoration1' className='section-decoration'>
-        <div id='section-decoration2' className='section-decoration'></div>
-      </div>
-    </div>
-  )
+function CreateList(props){
+  let list = [];
+  let num = 0;
+  props.users.forEach((user) => {
+    if (user.name.toLowerCase().includes(props.search.toLowerCase())) {
+      list.push(<User 
+      name = {user.name} 
+      email = {user.email} 
+      offer = {user.offer}
+      num = {num}
+      inList = {props.inList}
+      changeStatus = {(user) => { props.changeStatus(user)}}
+      />)
+    }
+    num += 1;
+  })
+  return list;
 }
 
 class App extends React.Component{
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      stage : 'stage1',
-      result : 0,
-      finished: 0,
-      len : Object.keys(questions).length,
-      end : false
+      users : users,
+      search : '',
+      isSend : false
     }
   }
 
-  checkAnswer(answ){
-    const stageNow = questions[this.state.stage]
-    let result = stageNow[stageNow.answer] == answ;
-    (result) ? result = 1 : result = 0;
-    this.setState((state) => ({
-      stage : Object.keys(questions)[state.finished+1],
-      finished : state.finished + 1,
-      result : state.result + result,
-    }))
-    console.log(this.state.finished, '  ', this.state.result);
-    if (this.state.finished == this.state.len-1) this.setState({end : true});
+
+  send(){
+      this.setState((state) => ({isSend : !state.isSend}))
+  }
+  changeStatus (user){
+    const users = this.state.users;
+    users[user].offer = !users[user].offer;
+    this.setState ({users : users});
   }
 
   render(){
-    if (this.state.end) return (
-      <End 
-      result={this.state.result / this.state.finished * 100} 
-      finished={(this.state.finished-1) / this.state.len * 100}
-        finish={() => setTimeout(() => progress.current.style.width = '100%', 10)}
-      />
-      );
-    const stage = questions[this.state.stage];
-    const qstns = [stage[1],stage[2],stage[3]];
-    return <Question 
-      click = {(answ) => this.checkAnswer(answ)} 
-      question={stage.question}
-      questions = {qstns}
-      finished = {this.state.finished/this.state.len*100}
-    />
+    if(this.state.isSend) {
+
+      let sendUsers = [];
+      this.state.users.forEach(user => {
+        console.log(user.offer);
+        if(user.offer == false) sendUsers.push(user);
+      });
+      console.log(sendUsers);
+
+      return (
+        <div className = 'users-list isSend'>
+          <div>Приглашения отправлены:</div>
+          <div className = 'list'>
+            <CreateList
+            inList = {false}
+            users={sendUsers}
+            search={''}
+            />
+          </div>
+          <button className='send' onClick={() => this.send()}>Назад</button>
+        </div>
+      )
+    }
+
+    return (
+      <div className = 'users-list'>
+        <div className = 'search-container' >
+          <img src= "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyi_CVTmoL1ITHFxQkfLwvj93hcsgA1Olkhg&usqp=CAU" className='search-img' />
+          <input 
+          type='text' 
+          className="user-search" 
+          placeholder="Найти пользователя"
+          onChange = {(e) => this.setState({search : e.target.value})}
+          value={this.state.search}
+          ></input>
+
+        </div>
+        <div className='list'>
+        <CreateList 
+        users = {this.state.users} 
+        search = {this.state.search}
+        inList = {true}
+        changeStatus = {(user) => {this.changeStatus(user)}}
+          />
+            
+        </div>
+        <button className = 'send' onClick = {() => this.send()}>Отправить приглашение</button>
+      </div>
+      )
   }
 }
 
